fdc00b867e085bc12ed60d370791719c814ffaf1
Varun Tekur <v_tekur@yahoo.com>
2022-05-14 22:43:29 -0400

    Update single_node_search.sh

--- dask_client_code/single_node_search.sh
+++ dask_client_code/single_node_search.sh
@@ -1,6 +1,6 @@
 for run in 1
 do
-    for cores in single_node_run_8.sh
+    for cores in single_node_run_8.sh single_node_run_4.sh single_node_run_1.sh
     do
         for index in {1..64}
         do
